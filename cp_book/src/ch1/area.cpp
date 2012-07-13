03b417c09ab204b3898a94e1dc92eb80fceb62a2
John M. Parrish <johnmparrish@gmail.com>
2012-07-13 16:32:40 -0400

    C++ implementation of the area program from section 2.3.

---
+++ cp_book/src/ch1/area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+const float pi = 3.14159265359;
+
+int main()
+{
+  double r, A;
+  std::cout << "Enter the radius of a circle" << std::endl;
+  std::cin >> r;
+  A = r * r * pi;
+  std::cout << "radius r = " << r << ", area A = " << A << std::endl;
+}
+
